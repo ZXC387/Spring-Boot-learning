1c93ac502c7c4c76cdc448df62a40dbd101131d2
zxc <512724412@qq.com>
2020-10-22 10:01:54 +0800

    helloword

---
+++ springboot-01-helloworld/src/main/java/com/zxc/HelloWorldMainApplication.java
@@ -0,0 +1,16 @@
+package com.zxc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zxc
+ * @date 2019-03-10 10:27
+ */
+@SpringBootApplication //标注一个主程序类，说明这是一个 Spring Boot应用
+public class HelloWorldMainApplication {
+    public static void main(String[] args) {
+        //Spring应用启动起来
+        SpringApplication.run(HelloWorldMainApplication.class,args);
+    }
+}
